4b914a76dfbdf191afeafc1753b94ffe5fa2f895
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-09-15 10:44:35 +0300

    started project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/root/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import configureStore from "./redux/reducers/configureStore"
+
+const store = configureStore()
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
