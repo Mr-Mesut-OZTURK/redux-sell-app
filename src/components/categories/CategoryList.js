1a81809872edad95a86a7c960651b8c216507d9a
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-09-16 12:00:26 +0300

    updated categoryList and download thunk to project

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
-export default class CategoryList extends Component {
+import { connect } from 'react-redux'
+import { bindActionCreators } from "redux"
+import * as categoryActions from "../../redux/actions/categoryActions"
+
+class CategoryList extends Component {
+
+    componentDidMount() {
+        this.props.actions.getCategories()
+        // console.log(this.props.categories)
+
+    }
+    
     render() {
+        // console.log(this.props.categories)
         return (
+            
             <div>
                 <h3>categories</h3>
+                <ListGroup>
+                    {
+
+                        this.props.categories.length > 0?
+                        (this.props.categories.map((category, index) => (
+                            
+                            <ListGroupItem
+                                active={this.props.currentCategory.id===category.id} 
+                                key={index}
+                                onClick={() => this.props.actions.changeCategory(category)}
+                            
+                            >
+                                {category.categoryName}
+                            </ListGroupItem>
+
+                        )))
+                        :
+                        ("")
+                    }
+                </ListGroup>
+                {/* <h5>seçili kategori : {this.props.currentCategory.categoryName}</h5> */}
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        currentCategory: state.changeCategoryReducer,
+        categories: state.categoryListReducer
+    }
+}
+
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: {
+            getCategories: bindActionCreators(categoryActions.getCategories, dispatch),
+            changeCategory: bindActionCreators(categoryActions.changeCategory, dispatch),
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList)
