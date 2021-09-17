5291f10bc68497de5fb7817e70792bd62bc81869
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-09-17 11:52:22 +0300

    created not found page

---
+++ src/components/common/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div style={{
+            minHeight:"80vh",
+            backgroundColor:"cornsilk",
+            display: "flex",
+            flexDirection:"column",
+            alignItems:"center",
+            justifyContent:"center"
+        }}>
+            <h2>Page Not Found!...</h2>
+        </div>
+    )
+}
+
+export default NotFound
